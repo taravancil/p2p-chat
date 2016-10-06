87b80a10df6f43329a605c5e2a7390aee2cb4386
Tara Vancil <tbvanc@gmail.com>
2016-10-06 11:06:26 -0400

    ignore node_modules

---
+++ 2-minimal-chat-server/README.md
@@ -0,0 +1,5 @@
+# 2 - Minimal chat server
+
+Write a server that sends whatever data it receives through the socket
+to all the clients connected to it except the one who sent the
+message.
---
+++ 2-minimal-chat-server/client.js
@@ -0,0 +1,13 @@
+const net = require('net')
+
+const socket = net.connect(3000, () => {
+  console.log('connected to the server!')
+})
+
+socket.on('data', (data) => {
+  process.stdout.write(data)
+})
+
+process.stdin.on('data', (data) => {
+  socket.write(data)
+})
