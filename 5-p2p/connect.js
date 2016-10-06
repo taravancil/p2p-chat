b243821ba9366a6e8e927e15d8d2cd2349df8d14
Tara Vancil <tbvanc@gmail.com>
2016-10-06 15:12:45 -0400

    use const not var

--- 5-p2p/connect.js
+++ 5-p2p/connect.js
@@ -4,7 +4,7 @@ const topology = require('fully-connected-topology')
 const me = process.argv[2]
 const peers = process.argv.slice(3)
 
-var t = topology(me, peers)
+const t = topology(me, peers)
 
 t.on('connection', (connection, peer) => {
   console.log(`connected to ${peer}`)
