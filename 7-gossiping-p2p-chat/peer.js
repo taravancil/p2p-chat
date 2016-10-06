8e486e2744de02bcbfbced8e53fd03b00cffd2e9
Tara Vancil <tbvanc@gmail.com>
2016-10-06 18:47:49 -0400

    improve readability

--- 7-gossiping-p2p-chat/peer.js
+++ 7-gossiping-p2p-chat/peer.js
@@ -10,20 +10,33 @@ const peerAddresses = process.argv.slice(4)
 const swarm = topology(me, peerAddresses)
 const activePeers = streamSet()
 
+// The index of the last message the peer has seen and forwarded to
+// its peers
+let lastMessageSeen = 0
+
 swarm.on('connection', (connection, peer) => {
   activePeers.add(connection)
   console.log(`new connection from ${peer}`)
 
   connection = jsonStream(connection)
   connection.on('data', (data) => {
+    // If the message ID > lastMessageSeen
+    // Increment the index and forward to all peers
+    // Else ignore the message
+    if (m)
     console.log(`${data.nickname}: ${data.message}`)
   })
 })
 
 const broadcast = (message) => {
   swarm.connections.forEach((connection) => {
+    const message = message.toString().trim()
+
     connection = jsonStream(connection)
-    connection.write({nickname: nickname, message: message.toString().trim()})
+    connection.write({
+      nickname: nickname,
+      message: message
+    })
   })
 }
 
