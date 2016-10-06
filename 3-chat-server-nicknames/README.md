a2251629dc8fc4d096827e49323251aeb323d623
Tara Vancil <tbvanc@gmail.com>
2016-10-06 14:01:20 -0400

    add challenge #3

---
+++ 3-chat-server-nicknames/README.md
@@ -0,0 +1,6 @@
+# 3 - Chat server with nicknames
+
+Add a feature such that when a client connects, it's prompted for a
+nickname. When a client sends a message to the server, it should
+include the nickname as well as the message and the server should
+relay the nickname along with all messages.
---
+++ 3-chat-server-nicknames/client.js
@@ -0,0 +1,19 @@
+'use strict'
+const jsonStream = require('duplex-json-stream')
+const net = require('net')
+
+const nickname = process.argv[2]
+
+let socket = net.connect(3000, () => {
+  console.log(`connected to the server!`)
+})
+
+socket = jsonStream(socket)
+
+socket.on('data', (data) => {
+  process.stdout.write(`${data['nickname']}: ${data.message}`)
+})
+
+process.stdin.on('data', (data) => {
+  socket.write({nickname: nickname, message: data.toString()})
+})
