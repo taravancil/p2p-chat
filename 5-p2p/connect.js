42709d341888b022af87531a68ca3cbd58291464
Tara Vancil <tbvanc@gmail.com>
2016-10-06 15:07:18 -0400

    complete #5

---
+++ 5-p2p/connect.js
@@ -0,0 +1,11 @@
+'use strict'
+const topology = require('fully-connected-topology')
+
+const me = process.argv[2]
+const peers = process.argv.slice(3)
+
+var t = topology(me, peers)
+
+t.on('connection', (connection, peer) => {
+  console.log(`connected to ${peer}`)
+})
