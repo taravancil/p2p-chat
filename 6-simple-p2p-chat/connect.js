83526ad5d95f2d1e2035550431dac00f59a654fd
Tara Vancil <tbvanc@gmail.com>
2016-10-06 15:31:27 -0400

    use dot notation

--- 6-simple-p2p-chat/connect.js
+++ 6-simple-p2p-chat/connect.js
@@ -7,7 +7,14 @@ const me = process.argv[2]
 const peers = process.argv.slice(3)
 
 const swarm = topology(me, peers)
+const activePeers = streamSet()
 
-swarm.on('connection', (connection, peer) => {
+swarm.on('connection', (socket, peer) => {
+  socket = jsonStream(socket)
+  activePeers.add(socket)
   console.log(`new connection from ${peer}`)
+
+  socket.on('data', (data) => {
+    process.stdout.write(`${data.nickname}: ${data.message}`)
+  })
 })
