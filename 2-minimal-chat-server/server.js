440501c601c5724c312f82b735212a106fc92f99
Tara Vancil <tbvanc@gmail.com>
2016-10-06 11:06:40 -0400

    add challenge #2

---
+++ 2-minimal-chat-server/server.js
@@ -0,0 +1,30 @@
+const net = require('net')
+const streamSet = require('stream-set')
+
+const activeSockets = streamSet()
+
+const server = net.createServer((socket) => {
+  activeSockets.add(socket)
+  showActiveConnections()
+
+  socket.on('close', () => {
+    showActiveConnections()
+  })
+
+  socket.on('data', (data) => {
+    activeSockets.forEach((stream) => {
+      // don't write to the socket from which the data originated
+      if (socket !== stream) {
+        stream.write(data)
+      }
+    })
+  })
+})
+
+const showActiveConnections = () => {
+  console.log(`${activeSockets.size} active connections`)
+}
+
+server.listen(3000, () => {
+  console.log('server listening on port 3000')
+})
