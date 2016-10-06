f938e64f31e614a1dffd9c6023dea383c6b6d2bd
Tara Vancil <tbvanc@gmail.com>
2016-10-06 14:41:32 -0400

    register alias

---
+++ 4-chat-with-discovery/server.js
@@ -0,0 +1,39 @@
+'use strict'
+const jsonStream = require('duplex-json-stream')
+const net = require('net')
+const register = require('register-multicast-dns')
+const streamSet = require('stream-set')
+
+register('test.local')
+
+const activeSockets = streamSet()
+
+const server = net.createServer((socket) => {
+  socket = jsonStream(socket)
+  activeSockets.add(socket)
+  showActiveConnections()
+
+  socket.on('end', () => {
+    showActiveConnections()
+  })
+
+  socket.on('data', (data) => {
+    broadcast(data, socket)
+  })
+})
+
+const broadcast = (data, sender) => {
+  activeSockets.forEach((client) => {
+    if (client === sender) return
+
+    client.write({ nickname: data.nickname, message: data.message})
+  })
+}
+
+const showActiveConnections = () => {
+  console.log(`${activeSockets.size} active connections`)
+}
+
+server.listen(3000, () => {
+  console.log('server listening on port 3000')
+})
